e1642f1182f411659c289f1784dab224f21c7c0b
Aniko Litvanyi <herflis33@gmail.com>
2018-01-22 11:28:54 +0100

    Remove sn-client-js and sn-redux related stuff

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import * as ReactDOM from 'react-dom'
 import {
   HashRouter as Router
 } from 'react-router-dom'
-import { combineReducers } from 'redux'
-import { Repository } from 'sn-client-js'
-import { Store, Reducers } from 'sn-redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { createLogger } from 'redux-logger';
+import { createEpicMiddleware } from 'redux-observable';
+// import { Repository } from 'sn-client-js'
+// import { Store, Reducers } from 'sn-redux'
 import { Provider } from 'react-redux';
 import { SNCommunityAppReducers } from './Reducers'
 import { SNCommunityAppEpics } from './Epics'
@@ -19,23 +21,31 @@ import './index.css'
 const issues = require('./data/issues.json');
 const pullrequests = require('./data/pullrequests.json');
 
-const sensenet = Reducers.sensenet
+// const sensenet = Reducers.sensenet
 const SNCommunityApp = SNCommunityAppReducers.SNCommunityApp
 
 const myReducer = combineReducers({
-  sensenet,
+  // sensenet,
   SNCommunityApp
 });
 
+const epicMiddleware = createEpicMiddleware(SNCommunityAppEpics.rootEpic);
+
+const logger = createLogger();
+
 const ids = Helpers.getSoIds();
 
-const repository = new Repository.SnRepository({
-  RepositoryUrl: process.env.REACT_APP_SERVICE_URL || 'https://dmsservice.demo.sensenet.com',
-  RequiredSelect: ['Id', 'Path', 'Name', 'Type', 'ParentId', 'Actions'],
-  DefaultExpand: ['Actions']
-});
+// const repository = new Repository.SnRepository({
+//   RepositoryUrl: process.env.REACT_APP_SERVICE_URL || 'https://dmsservice.demo.sensenet.com',
+//   RequiredSelect: ['Id', 'Path', 'Name', 'Type', 'ParentId', 'Actions'],
+//   DefaultExpand: ['Actions']
+// });
 
-const store = Store.configureStore(myReducer, SNCommunityAppEpics.rootEpic, undefined, {}, repository)
+let store = createStore(
+  myReducer,
+  {},
+  applyMiddleware(logger, epicMiddleware)
+);
 store.dispatch(SNCommunityAppActions.getSOStats(ids))
 store.dispatch(SNCommunityAppActions.getSOSNTagAnswerers())
 store.dispatch(SNCommunityAppActions.getSOQuestions())
