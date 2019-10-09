e08326484b52cf6445c9248478aa07e457146166
Aditya <adityay@mindfiresolutions.com>
2019-10-09 16:07:35 +0530

    Basic syntax fix

--- src/components/Lists/User.tsx
+++ src/components/Lists/User.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react'
-import {
-  Link
-} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { ListItem, ListItemText, ListItemIcon } from 'material-ui/List'
 import Radium from 'radium'
 import Avatar from 'material-ui/Avatar'
